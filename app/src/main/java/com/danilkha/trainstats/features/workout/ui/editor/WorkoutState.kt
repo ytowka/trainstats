45298ca9153387c0367d5ab36305aa2eb7461e27
Danil Khairulin <enddonil@gmail.com>
2026-01-23 15:21:07 +0300

    fix last updated date erase

--- app/src/main/java/com/danilkha/trainstats/features/workout/ui/editor/WorkoutState.kt
+++ app/src/main/java/com/danilkha/trainstats/features/workout/ui/editor/WorkoutState.kt
@@ -39,7 +39,8 @@ data class WorkoutState(
                 weight = set.weight,
                 exerciseName = group.name
             )
-        }
+        },
+        lastEdited = lastEdited
     )
 }
 
