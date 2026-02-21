8d60aff47a56f7a0bf75af4f88fd36f1d31756c5
Danil Khairulin <enddonil@gmail.com>
2026-02-21 14:33:56 +0300

    startup benchmark

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,12 @@ android {
                 "proguard-rules.pro"
             )
         }
+        create("benchmark") {
+            initWith(buildTypes.getByName("release"))
+            signingConfig = signingConfigs.getByName("debug")
+            matchingFallbacks += listOf("release")
+            isDebuggable = false
+        }
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
--- build.gradle.kts
+++ build.gradle.kts
@@ -16,5 +16,6 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.android.lint) apply false
+    alias(libs.plugins.android.test) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
